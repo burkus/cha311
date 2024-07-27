4a7b770f37c0269908ad182c0508238c7e2cf9d7
Andrew Burkus <da8urk@gmail.com>
2024-07-27 14:33:36 -0400

    clean up

--- src/lib/311request.ts
+++ src/lib/311request.ts
@@ -1,11 +1,5 @@
-import { PublicParkWorkRequestInputs, createPublicParkWorkRequest } from "../types/requests"
 import { Cha311URL } from "./const"
 
-const formify = (data: Record<string, unknown>) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((key) => formData.append(key, data[key] as string))
-    return formData;
-}
 
 export const createParks311Request = async (form: FormData) => {
     return await fetch(Cha311URL, {
